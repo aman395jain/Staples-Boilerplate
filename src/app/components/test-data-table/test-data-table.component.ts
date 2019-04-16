13e52a2b55ab7e489aefe8254b444462c6228aec
Aman Jain <AmanJa@hcl.com>
2019-04-16 14:31:54 +0530

    test data table changes

--- src/app/components/test-data-table/test-data-table.component.ts
+++ src/app/components/test-data-table/test-data-table.component.ts
@@ -10,6 +10,7 @@ import "rxjs/add/observable/of";
 
 import { LogDiscriptionComponent } from "../log-discription/log-discription.component";
 import { LoglistingService } from "src/app/services/log-listing/loglisting.service";
+import { NavBarService } from "src/app/services/nav-bar/nav-bar.service";
 
 @Component({
   selector: "app-test-data-table",
@@ -20,47 +21,52 @@ export class TestDataTableComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  columns = [
-    {
-      columnDef: "sku",
-      header: "SKU",
-      cell: (element: any) => `${element.sku}`
-    },
-    {
-      columnDef: "itemDesc",
-      header: "Item Description",
-      cell: (element: any) => `${element.itemDesc}`
-    },
-    {
-      columnDef: "permPrice",
-      header: "Perm Price",
-      cell: (element: any) => `${element.permPrice}`
-    },
-    {
-      columnDef: "posId",
-      header: "Position ID",
-      cell: (element: any) => `${element.posId}`
-    },
-    {
-      columnDef: "barCode",
-      header: "Bar Code",
-      cell: (element: any) => `${element.posId}`
-    },
-    {
-      columnDef: "action",
-      header: "",
-      cell: (element: any) => null
-    }
-  ];
+  columns = [];
 
-  displayedColumns = this.columns.map(c => c.columnDef);
+  displayedColumns = {};
   dataByAPI: MatTableDataSource<any>;
   constructor(
     private _loglistingService: LoglistingService,
-    private _dialog: MatDialog
+    private _dialog: MatDialog,
+    private _navBarService: NavBarService
   ) {}
 
   ngOnInit() {
+    this.columns = [
+      {
+        columnDef: "sku",
+        header: "SKU",
+        cell: (element: any) => `${element.sku}`
+      },
+      {
+        columnDef: "itemDesc",
+        header: "Item Description",
+        cell: (element: any) => `${element.itemDesc}`
+      },
+      {
+        columnDef: "permPrice",
+        header: "Perm Price",
+        cell: (element: any) => `${element.permPrice}`
+      },
+      {
+        columnDef: "posId",
+        header: "Position ID",
+        cell: (element: any) => `${element.posId}`
+      },
+      {
+        columnDef: "barCode",
+        header: "Bar Code",
+        cell: (element: any) => `${element.posId}`
+      },
+      {
+        columnDef: "action",
+        header: "",
+        cell: (element: any) => null
+      }
+    ];
+
+    this.displayedColumns = this.columns.map(c => c.columnDef);
+
     try {
       this._loglistingService.getLogList().subscribe(data => {
         this.dataByAPI = new MatTableDataSource(data);
@@ -70,6 +76,14 @@ export class TestDataTableComponent implements OnInit {
     } catch (e) {
       console.log("in the test error", e);
     }
+    this._navBarService.getElementName().subscribe(tableName => {
+      console.log("in the test data table", tableName);
+      this._loglistingService.getLogListForEntity(tableName).subscribe(data => {
+        this.dataByAPI = new MatTableDataSource(data);
+        this.dataByAPI.sort = this.sort;
+        this.dataByAPI.paginator = this.paginator;
+      });
+    });
   }
 
   /*
@@ -92,5 +106,12 @@ export class TestDataTableComponent implements OnInit {
     _dialogConfig.autoFocus = true;
     _dialogConfig.width = "60%";
     this._dialog.open(LogDiscriptionComponent, _dialogConfig);
+  }
+
+  isSortingDisabled(columnHeader) {
+    if (columnHeader === "Bar Code") {
+      return true;
+    }
+    return false;
   }
 }
