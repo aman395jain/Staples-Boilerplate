c70a6c0d73aad971535d87354defdc85a0c7f2a4
aman395jain <er.amanjain395@gmail.com>
2019-04-17 22:26:55 +0530

    changes for keys in Modal

--- src/app/components/log-discription/log-discription.component.ts
+++ src/app/components/log-discription/log-discription.component.ts
@@ -8,25 +8,54 @@ import { MAT_DIALOG_DATA } from "@angular/material";
 })
 export class LogDiscriptionComponent implements OnInit {
   dataDiscriptionKeysToDisplay = [];
-  testArray1 = [];
+  dataDisplayOnModal = [];
+  keyMap = {};
   constructor(@Inject(MAT_DIALOG_DATA) public dataDiscription: any) {}
 
   ngOnInit() {
     this.dataDiscriptionKeysToDisplay.push(this.dataDiscription);
-    let testData = this.dataDiscriptionKeysToDisplay.map(data => ({
-      STORE: data.store,
-      SKU: data.sku,
-      "ITEM DESCRIPTION": data.itemDesc,
-      "PERM PRICE": data.permPrice,
-      "POSITION ID": data.posId,
-      "BAR CODE": data.barCode,
-      "SELL PRICE": data.sellPrice,
-      PROFIT: data.profit,
-      STATE: data.state
-    }));
 
-    this.testArray1.push(testData[0]);
+    // this.dataDisplayOnModal = this.dataDiscriptionKeysToDisplay.map(data => ({
+    //   STORE: data.store,
+    //   SKU: data.sku,
+    //   "ITEM DESCRIPTION": data.itemDesc,
+    //   "PERM PRICE": data.permPrice,
+    //   "POSITION ID": data.posId,
+    //   "BAR CODE": data.barCode,
+    //   "SELL PRICE": data.sellPrice,
+    //   PROFIT: data.profit,
+    //   STATE: data.state
+    // }));
 
-    console.log("in the LogDiscriptionComponent", testData[0]);
+    // this.dataDisplayOnModal.map(data => {
+    //   console.log("in the map", Object.keys(data));
+    // });
+
+    // console.log("in the log component", this.dataDiscription);
+
+    this.keyMap = {
+      barCode: "Bar Code",
+      itemDesc: "Item Description",
+      permPrice: "Prem Price",
+      posId: "POSITION ID",
+      profit: "PROFIT",
+      sellPrice: "SELL PRICE",
+      sku: "SKU",
+      state: "STATE",
+      store: "STORE"
+    };
+
+    this.dataDisplayOnModal = this.dataDiscriptionKeysToDisplay.map(data => {
+      return Object.keys(data).reduce((prev, next) => {
+        if (next in this.keyMap) {
+          prev[this.keyMap[next]] = data[next];
+        } else {
+          prev[next] = data[next];
+        }
+        return prev;
+      }, {});
+    });
+
+    console.log("in the log component", this.dataDisplayOnModal);
   }
 }
