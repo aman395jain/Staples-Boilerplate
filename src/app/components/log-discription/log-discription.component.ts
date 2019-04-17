a69c22e6a4bcb835a881a7f0ae65743b0264fb6b
Aman Jain <AmanJa@hcl.com>
2019-04-17 17:13:50 +0530

    mapping the the objects in modal

--- src/app/components/log-discription/log-discription.component.ts
+++ src/app/components/log-discription/log-discription.component.ts
@@ -7,9 +7,64 @@ import { MAT_DIALOG_DATA } from "@angular/material";
   styleUrls: ["./log-discription.component.scss"]
 })
 export class LogDiscriptionComponent implements OnInit {
+  dataDiscriptionKeys = [];
+  dataDiscriptionKeysToDisplay = [];
+
   constructor(@Inject(MAT_DIALOG_DATA) public dataDiscription: any) {}
 
   ngOnInit() {
-    console.log("in the LogDiscriptionComponent", this.dataDiscription);
+    this.dataDiscriptionKeys = Object.keys(this.dataDiscription);
+    this.dataDiscriptionKeys.map(ele => {
+      if (ele === "store") {
+        this.dataDiscriptionKeysToDisplay.push({
+          STORE: this.dataDiscription.store
+        });
+      }
+      if (ele === "sku") {
+        this.dataDiscriptionKeysToDisplay.push({
+          SKU: this.dataDiscription.sku
+        });
+      }
+      if (ele === "itemDesc") {
+        this.dataDiscriptionKeysToDisplay.push({
+          "ITEM DESCRIPTION": this.dataDiscription.itemDesc
+        });
+      }
+      if (ele === "permPrice") {
+        this.dataDiscriptionKeysToDisplay.push({
+          "PERM PRICE": this.dataDiscription.permPrice
+        });
+      }
+      if (ele === "posId") {
+        this.dataDiscriptionKeysToDisplay.push({
+          "POSITION ID": this.dataDiscription.posId
+        });
+      }
+      if (ele === "barCode") {
+        this.dataDiscriptionKeysToDisplay.push({
+          "BAR CODE": this.dataDiscription.barCode
+        });
+      }
+      if (ele === "sellPrice") {
+        this.dataDiscriptionKeysToDisplay.push({
+          "SELL PRICE": this.dataDiscription.sellPrice
+        });
+      }
+      if (ele === "profit") {
+        this.dataDiscriptionKeysToDisplay.push({
+          PROFIT: this.dataDiscription.profit
+        });
+      }
+      if (ele === "state") {
+        this.dataDiscriptionKeysToDisplay.push({
+          STATE: this.dataDiscription.state
+        });
+      }
+    });
+
+    console.log(
+      "in the LogDiscriptionComponent",
+      this.dataDiscriptionKeysToDisplay
+    );
   }
 }
