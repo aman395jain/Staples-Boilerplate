30b1b9094fc353a69bfa794742e7a3e597fe4f6c
Aman Jain <AmanJa@hcl.com>
2019-04-23 10:45:46 +0530

    adding mock services

--- src/app/services/log-listing/loglisting.service.ts
+++ src/app/services/log-listing/loglisting.service.ts
@@ -38,6 +38,26 @@ export class LoglistingService {
     // console.log("in the loglist service", entity);
     if (entity === "Price_Prompt_SKUs") {
       this._serviceUrl = "http://www.mocky.io/v2/5cac66ee300000b723103596";
+    } else if (entity === "Employee") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cac66ee300000b723103596";
+    } else if (entity === "Tax_Rates") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cac66ee300000b723103596";
+    } else if (entity === "Hardware_SKUs") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d";
+    } else if (entity === "Linked_SKUs") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d";
+    } else if (entity === "Free_SKUs") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d";
+    } else if (entity === "Age_Restricted_Special_rest") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d";
+    } else if (entity === "Return_Driver_License") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d";
+    } else if (entity === "Lowest_Price") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d";
+    } else if (entity === "POSA") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d";
+    } else if (entity === "Order") {
+      this._serviceUrl = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d";
     } else {
       this._serviceUrl = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d";
     }
