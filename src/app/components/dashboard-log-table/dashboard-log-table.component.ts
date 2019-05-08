ec3c3f7bbdaee7b0ed3531088a0c15f06412ef7e
Aman Jain <AmanJa@hcl.com>
2019-05-08 14:19:30 +0530

    strectable left bar

--- src/app/components/dashboard-log-table/dashboard-log-table.component.ts
+++ src/app/components/dashboard-log-table/dashboard-log-table.component.ts
@@ -9,17 +9,22 @@ import { NavBarService } from "src/app/services/nav-bar/nav-bar.service";
 export class DisplayLogTableComponent implements OnInit {
   splitLeftBarSize = "5";
   rightPartSplitSize = "95";
+  minLeftBarSize = "5";
+  maxLeftBarSize = "5";
   constructor(private _navBarService: NavBarService) {}
 
   ngOnInit() {
     this._navBarService.getToggleStatus().subscribe(toggleStatus => {
-      console.log("in the dashboard", toggleStatus);
       if (toggleStatus) {
         this.splitLeftBarSize = "12";
         this.rightPartSplitSize = "88";
+        this.minLeftBarSize = "12";
+        this.maxLeftBarSize = "20";
       } else {
         this.splitLeftBarSize = "5";
         this.rightPartSplitSize = "95";
+        this.minLeftBarSize = "5";
+        this.maxLeftBarSize = "5";
       }
     });
   }
