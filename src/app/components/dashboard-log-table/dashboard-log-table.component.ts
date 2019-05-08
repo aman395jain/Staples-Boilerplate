7aaf181327aff47a273583ee90cf631dd28e07a8
Aman Jain <AmanJa@hcl.com>
2019-05-08 13:40:22 +0530

    strectchable side nav bar

--- src/app/components/dashboard-log-table/dashboard-log-table.component.ts
+++ src/app/components/dashboard-log-table/dashboard-log-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { NavBarService } from "src/app/services/nav-bar/nav-bar.service";
 
 @Component({
   selector: "app-dashboard-log-table",
@@ -6,10 +7,20 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./dashboard-log-table.component.scss"]
 })
 export class DisplayLogTableComponent implements OnInit {
-  splitSize = "";
-  constructor() {}
+  splitLeftBarSize = "5";
+  rightPartSplitSize = "95";
+  constructor(private _navBarService: NavBarService) {}
 
   ngOnInit() {
-    this.splitSize = "12";
+    this._navBarService.getToggleStatus().subscribe(toggleStatus => {
+      console.log("in the dashboard", toggleStatus);
+      if (toggleStatus) {
+        this.splitLeftBarSize = "12";
+        this.rightPartSplitSize = "88";
+      } else {
+        this.splitLeftBarSize = "5";
+        this.rightPartSplitSize = "95";
+      }
+    });
   }
 }
