c1239eb4f808224f49d21347e0b59dc292e3b363
AMAN JAIN <er.amanjain395@gmail.com>
2019-06-25 17:29:33 +0530

    Update logTableApiUrls.enum.ts
    minor changes

--- src/app/utils/logTableApiUrls.enum.ts
+++ src/app/utils/logTableApiUrls.enum.ts
@@ -17,5 +17,6 @@ export enum logTableAPIUrls {
   getOrder = "http://www.mocky.io/v2/5d0393333200004b00d7474b",
   getPromos = "http://lrtdqnasv104:8090/tdmapp/promoList",
   getItemGroup = "http://www.mocky.io/v2/5cf60cd9320000ac2b8ccf7c",
-  getTaxExempt = "http://www.mocky.io/v2/5cffb39f3200005400eac913"
+  getTaxExempt = "http://www.mocky.io/v2/5cffb39f3200005400eac913",
+  getBagFeeSKUs = "http://www.mocky.io/v2/5d11f56b310000938008cfda"
 }
