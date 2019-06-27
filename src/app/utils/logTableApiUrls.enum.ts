2ab17bfebed6b203a39a8928ee436057938415a1
Ridhi Gulati <RidhiG@hcl.com>
2019-06-27 13:55:20 +0530

    Advanced Search

--- src/app/utils/logTableApiUrls.enum.ts
+++ src/app/utils/logTableApiUrls.enum.ts
@@ -2,21 +2,20 @@
  * API's url to call data from Restful api's for different data-logs.
  */
 export enum logTableAPIUrls {
-  getItemMaster = "http://lrtdqnasv104:8090/tdmapp/itemMstList",
-  getPricePromptSKUs = "http://lrtdqnasv104:8090/tdmapp/pricePromptList",
-  getItemMasterMain = "http://lrtdqnasv104:8090/tdmapp/itemMstList",
-  getEmployee = "http://lrtdqnasv104:8090/tdmapp/employeeList",
-  getTaxRate = "http://lrtdqnasv104:8090/tdmapp/taxList",
-  getHardwareSKUs = "http://lrtdqnasv104:8090/tdmapp/itmMstHdwSkuList",
-  getLinkedSKUs = "http://lrtdqnasv104:8090/tdmapp/linkedSkuList",
+  getItemMaster = "http://www.mocky.io/v2/5ccff0b9320000b52100f912",
+  getPricePromptSKUs = "http://www.mocky.io/v2/5ccff0b9320000b52100f912",
+  getItemMasterMain = "http://www.mocky.io/v2/5cf4ddba2f000015354f04f8",
+  getEmployee = "http://www.mocky.io/v2/5cf4c7832f000070224f0459",
+  getTaxRate = "http://www.mocky.io/v2/5cf4c9db2f000099264f0469",
+  getHardwareSKUs = "http://www.mocky.io/v2/5cf50a002f000070224f063d",
+  getLinkedSKUs = "http://www.mocky.io/v2/5ceba50c33000070006d7603",
   getFreeSKUs = "http://www.mocky.io/v2/5cbeff80300000bb069ce463",
-  getAgeRestrictiedSpecialRest = "http://lrtdqnasv104:8090/tdmapp/ageRestrictedList",
+  getAgeRestrictiedSpecialRest = "http://www.mocky.io/v2/5cd9297b3000006d21c0143e",
   getReturnDrivingLicence = "http://www.mocky.io/v2/5cbff4ce310000170e035f4e",
   getLowestPrice = "http://www.mocky.io/v2/5cbffef9310000580b035f87",
   getPOSA = "http://www.mocky.io/v2/5cb860794c0000c51ad3d50d",
   getOrder = "http://www.mocky.io/v2/5d0393333200004b00d7474b",
-  getPromos = "http://lrtdqnasv104:8090/tdmapp/promoList",
+  getPromos = "http://www.mocky.io/v2/5ce6717d3300005600731588",
   getItemGroup = "http://www.mocky.io/v2/5cf60cd9320000ac2b8ccf7c",
-  getTaxExempt = "http://www.mocky.io/v2/5cffb39f3200005400eac913",
-  getBagFeeSKUs = "http://www.mocky.io/v2/5d11f56b310000938008cfda"
+  getTaxExempt = "http://www.mocky.io/v2/5cffb39f3200005400eac913"
 }
